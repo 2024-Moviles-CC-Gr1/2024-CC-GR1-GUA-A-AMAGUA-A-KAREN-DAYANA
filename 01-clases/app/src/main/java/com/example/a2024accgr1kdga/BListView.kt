c73e33c44b39d6c432815b3a9feda3dcdfe2c9f7
KarenEPN <karen.guana22@gmail.com>
2024-06-26 13:29:30 -0500

    ultimos cambios

--- 01-clases/app/src/main/java/com/example/a2024accgr1kdga/BListView.kt
+++ 01-clases/app/src/main/java/com/example/a2024accgr1kdga/BListView.kt
@@ -1,5 +1,6 @@
 package com.example.a2024accgr1kdga
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
@@ -7,10 +8,10 @@ import android.view.MenuItem
 import android.view.View
 import android.view.View.OnCreateContextMenuListener
 import android.widget.AdapterView
-import android.widget.AdapterView.AdapterContextMenuInfo
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
@@ -20,8 +21,8 @@ class BListView : AppCompatActivity() {
         setContentView(R.layout.activity_blist_view)
         val listView = findViewById<ListView>(R.id.lv_list_view)
         val adaptador = ArrayAdapter(
-            this, // Contexto
-            android.R.layout.simple_list_item_1, //layout xml a usar
+            this, // contexto
+            android.R.layout.simple_list_item_1, // layout xml a usar
             arreglo
         )
         listView.adapter = adaptador
@@ -30,53 +31,106 @@ class BListView : AppCompatActivity() {
         val botonAnadirListView = findViewById<Button>(
             R.id.btn_anadir_list_view
         )
-        botonAnadirListView.setOnClickListener{
+        botonAnadirListView.setOnClickListener {
             anadirEntrenador(adaptador)
         }
-        registerForContextMenu(listView)
-    }
-    // SE ACABA EL ON CREATE
+        registerForContextMenu(listView) // NUEVA LINEA
+    } // ACABA EL ON CREATE
     var posicionItemSeleccionado = -1
     override fun onCreateContextMenu(
         menu: ContextMenu?,
         v: View?,
         menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
+    ){
+        super.onCreateContextMenu(menu,v,menuInfo)
+        // llenamos opciones del menu
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
+        // Obtener id
         val info = menuInfo as AdapterView.AdapterContextMenuInfo
         val posicion = info.position
         posicionItemSeleccionado = posicion
     }
-
     override fun onContextItemSelected(
         item: MenuItem): Boolean {
         return when (item.itemId){
-            R.id.mi_editar ->{
+            R.id.mi_editar -> {
                 mostrarSnackbar("Editar $posicionItemSeleccionado")
                 return true
             }
-            R.id.mi_eliminar ->{
+            R.id.mi_eliminar -> {
                 mostrarSnackbar("Eliminar $posicionItemSeleccionado")
+                abrirDialogo()// NUEVA LINEA
                 return true
             }
             else -> super.onContextItemSelected(item)
         }
     }
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea Eliminar")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener {
+                    dialogInterface, i ->
+                mostrarSnackbar("Eliminar Aceptado")
+            }
+        )
+        builder.setNegativeButton("Cancelar", null)
+        val opciones = resources.getStringArray(
+            R.array.string_array_opciones
+        )
+        val seleccionPrevia = booleanArrayOf(
+            true, false, false
+        )
+        builder.setMultiChoiceItems(
+            opciones, seleccionPrevia,
+            {   dialog, which, isChecked->
+                mostrarSnackbar("Dio click en el item $which")
+            }
+        )
+        val dialogo = builder.create()
+        dialogo.show()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     fun anadirEntrenador(adaptador:ArrayAdapter<BEntrenador>){
         arreglo.add(
-            BEntrenador(4, "Wendy","w@w.com")
+            BEntrenador(4,"Wendy","d@d.com")
         )
         adaptador.notifyDataSetChanged()
     }
     fun mostrarSnackbar(texto:String){
         val snack = Snackbar.make(
-            findViewById(R.id.cl_ciclo_vida),
+            findViewById(R.id.cl_blist_view),
             texto,
             Snackbar.LENGTH_INDEFINITE
         )
         snack.show()
     }
+
+
+
+
 }
