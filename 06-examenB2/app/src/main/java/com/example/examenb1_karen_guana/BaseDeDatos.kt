cdc985d6cd6663b0637e00a67531304db400d9ef
KarenEPN <karen.guana22@gmail.com>
2024-08-02 10:22:28 -0500

    examen

---
+++ 06-examenB2/app/src/main/java/com/example/examenb1_karen_guana/BaseDeDatos.kt
@@ -0,0 +1,7 @@
+package com.example.examenb1_karen_guana
+
+class BaseDeDatos {
+    companion object{
+        var tablas: ESqliteHelperExamen? = null
+    }
+}
---
+++ 06-examenB2/app/src/main/java/com/example/examenb1_karen_guana/CrearAplicacion.kt
@@ -0,0 +1,91 @@
+package com.example.examenb1_karen_guana
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class CrearAplicacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_aplicacion)
+
+        val idSO = intent.getIntExtra("id", 0)
+        val nombreSo = intent.getStringExtra("nombre")
+
+        val botonCrearAplicacion = findViewById<Button>(R.id.btn_crear_Aplicacion)
+        botonCrearAplicacion
+            .setOnClickListener {
+                val nombre = findViewById<EditText>(R.id.input_nombre_aplicacion)
+                val version = findViewById<EditText>(R.id.input_version_aplicacion)
+                val tamanoMb = findViewById<EditText>(R.id.input_tamano)
+                val fechaLanzamiento = findViewById<EditText>(R.id.input_fecha_aplicacion)
+                val categoria = findViewById<EditText>(R.id.input_categoria)
+
+                val (exito,idAplicacion)  = BaseDeDatos
+                    .tablas!!.crearAplicacion(
+
+                        nombre.text.toString(),
+                        version.text.toString(),
+                        tamanoMb.text.toString().toInt(),
+                        fechaLanzamiento.text.toString(),
+                        categoria.text.toString(),
+                        idSO
+                    )
+
+
+                if (exito) {
+
+                    crearAplicacion(
+                        idAplicacion.toString(),
+                        idSO.toString(),
+                        nombre.text.toString(),
+                        version.text.toString(),
+                        tamanoMb.text.toString().toInt(),
+                        fechaLanzamiento.text.toString(),
+                        categoria.text.toString())
+
+                    val intent = Intent(this, AListView::class.java)
+                    intent.putExtra("id", idSO)
+                    intent.putExtra("nombre", nombreSo)
+                    startActivity(intent)
+                }
+            }
+
+
+
+    }
+
+    fun crearAplicacion(
+        id:String,
+        idSO: String,
+        nombre: String,
+        version: String,
+        tamanoMb: Int,
+        fechaLanzamiento: String,
+        categoria: String
+    ){
+        val db = Firebase.firestore
+        val referenciaAplicacion = db.collection("SistemaOperativo").document(idSO)
+            .collection("Aplicacion")
+
+        val datosAplicacion = hashMapOf(
+            "nombre" to nombre,
+            "version" to version,
+            "tamanoMb" to tamanoMb,
+            "fechaLanzamiento" to fechaLanzamiento,
+            "categoria" to categoria
+        )
+
+        // identificador quemado (crear/actualizar)
+        referenciaAplicacion
+            .document(id)
+            .set(datosAplicacion)
+            .addOnSuccessListener {  }
+            .addOnFailureListener {  }
+    }
+
+}
